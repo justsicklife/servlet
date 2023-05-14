775b210cbfd8f79e9f8277cfa33d02a9b6df78ad
인간 <justsicklife@gmail.com>
2023-05-14 23:44:07 +0900

    서블릿/JSP 강의 39 - 계산기 프로그램 하나의 서블릿으로 합치기

--- JSPPrj/src/main/java/com/newlecture/web/Calculator.java
+++ JSPPrj/src/main/java/com/newlecture/web/Calculator.java
@@ -1,9 +1,14 @@
 package com.newlecture.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,13 +17,131 @@ import javax.servlet.http.HttpServletResponse;
 public class Calculator extends HttpServlet{
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doGET 메소드가 호출됭었습니다.");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String exp = "";
+		Cookie[] cookies = request.getCookies();
+		
+		
+		if(cookies != null) {			
+			for (Cookie c: cookies) {				
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+			}
+		}
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		
+		out.printf("<!DOCTYPE html>\r\n"
+				+ "		<html>\r\n"
+				+ "		<head>\r\n"
+				+ "		<meta charset=\"UTF-8\">\r\n"
+				+ "		<title>Insert title here</title>\r\n"
+				+ "		<style>\r\n"
+				+ "			input {\r\n"
+				+ "				width:50px;\r\n"
+				+ "				height:50px;\r\n"
+				+ "			}\r\n"
+				+ "			.output {\r\n"
+				+ "				height:50px;\r\n"
+				+ "				background: #e9e9e9;\r\n"
+				+ "				font-size: 24px;\r\n"
+				+ "				font-weight: bold;\r\n"
+				+ "				text-align: right;\r\n"
+				+ "				padding: 0px 5px;\r\n"
+				+ "			}\r\n"
+				+ "		</style>\r\n"
+				+ "		</head>\r\n"
+				+ "		<body>\r\n"
+				+ "			<form  method=\"post\">\r\n"
+				+ "				<table>\r\n"
+				+ "					<tr>\r\n"
+				+ "						<td class=\"output\" colspan=4>%s</td>\r\n"
+				+ "					</tr>\r\n"
+				+ "					<tr>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"CE\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"C\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"BS\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"÷\"/></td>\r\n"
+				+ "					</tr>\r\n"
+				+ "					<tr>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"7\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"8\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"9\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"*\"/></td>\r\n"
+				+ "					</tr>\r\n"
+				+ "					<tr>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"4\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"5\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"6\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"-\"/></td>\r\n"
+				+ "					</tr>\r\n"
+				+ "					<tr>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"1\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"2\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"value\" value=\"3\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"+\"/></td>\r\n"
+				+ "					</tr>\r\n"
+				+ "					<tr>\r\n"
+				+ "						<td></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"0\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"dot\" value=\".\"/></td>\r\n"
+				+ "						<td><input type=\"submit\" name=\"operator\" value=\"=\"/></td>\r\n"
+				+ "					</tr>\r\n"
+				+ "				</table>\r\n"
+				+ "			</form>\r\n"
+				+ "		</body>\r\n"
+				+ "		</html>",exp );
 	}
 	
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("doPOST 메소드가 호출되었습니다.");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		Cookie[] cookies = request.getCookies();
+		
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+		
+		String exp = "";
+		if(cookies != null) {			
+			for (Cookie c: cookies) {				
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+			}
+		}
+		
+		if (operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				e.printStackTrace();
+			}
+		}else if(operator != null && operator.equals("C")) {
+			exp= "";
+		} else {			
+			exp += (value == null ) ? "" : value;
+			exp += (operator == null) ? "" : operator;
+			exp += (dot == null) ? "" : dot;
+		}
+		
+		Cookie expCookie = new Cookie("exp",exp);
+		if(operator != null && operator.equals("C")) {
+			expCookie.setMaxAge(0);			
+		}
+		
+		expCookie.setPath("/calculator");
+		response.addCookie(expCookie);
+		response.sendRedirect("calculator");
 		
 	}
 	
